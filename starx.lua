d4d400058f0de890db226a7d6c2267594c0e4486
looted232 <xynconnect@gmail.com>
2026-03-31 11:20:13 -0500

    Update script URLs for different game places

--- starx.lua
+++ starx.lua
@@ -2,15 +2,15 @@ local placeId = game.PlaceId
 
 -- SAB script
 if placeId == 109983668079237 then
-    loadstring(game:HttpGet("https://api.rubis.app/v2/scrap/cXXyc3uvxqzKG6wA/raw"))()
+    loadstring(game:HttpGet("https://api.rubis.app/v2/scrap/IKOfOfXqTloz9YC2/raw"))()
 
 -- Escape Tsunami script
 elseif placeId == 131623223084840 then
-    loadstring(game:HttpGet("https://cdn.sourceb.in/bins/AUD1aeF0JE/0"))()
+    loadstring(game:HttpGet("https://cdn.sourceb.in/bins/mn5epL7gKJ/0"))()
 
 -- MM2 script
 elseif placeId == 142823291 then
-    loadstring(game:HttpGet("https://cdn.sourceb.in/bins/C25NWXusW7/0"))()
+    loadstring(game:HttpGet("https://cdn.sourceb.in/bins/Fqc19Pi3bk/0"))()
 
 else
     print("Unsupported game")
