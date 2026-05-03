fb7e5f2011c50e60f62ea256def2f31423f99095
looted232 <xynconnect@gmail.com>
2026-05-02 19:40:31 -0500

    Update script URL in zero2.lua

--- zero2.lua
+++ zero2.lua
@@ -1,2 +1,2 @@
 --- Do Not Use Alt!!
-loadstring(game:HttpGet("https://api.rubis.app/v2/scrap/PS5fI1tYQNd1YTN8/raw"))()
+loadstring(game:HttpGet("https://api.rubis.app/v2/scrap/1IX0phppALGF5wEZ/raw"))()
